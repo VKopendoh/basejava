0fcbd65bc6461c7147e93688cc45074052067a1d
Vladimir Kopendoh <vkopendoh@gmail.com>
2019-06-19 11:39:12 +0300

    HW-4: JUnit update test repair.

--- test/com/vkopendoh/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/vkopendoh/webapp/storage/AbstractArrayStorageTest.java
@@ -70,8 +70,9 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void update() {
-        storage.update(RESUME_1);
-        Assert.assertSame(RESUME_1, storage.get(UUID_1));
+        Resume resume = new Resume(UUID_1);
+        storage.update(resume);
+        Assert.assertSame(resume, storage.get(UUID_1));
     }
 
     @Test(expected = NotExistStorageException.class)
