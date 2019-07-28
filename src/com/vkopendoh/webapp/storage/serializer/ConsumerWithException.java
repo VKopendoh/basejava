df91af4447c911a7b4194efc5815b0b40dc88644
vkopendoh <vkopendoh@bk.ru>
2019-07-28 18:58:34 +0300

    HW10: DataStreamSerialize without generics

--- src/com/vkopendoh/webapp/storage/serializer/ConsumerWithException.java
+++ src/com/vkopendoh/webapp/storage/serializer/ConsumerWithException.java
@@ -3,6 +3,6 @@ package com.vkopendoh.webapp.storage.serializer;
 import java.io.IOException;
 
 @FunctionalInterface
-public interface ConsumerWithException<T> {
-    void accept(T t) throws IOException;
+public interface ConsumerWithException {
+    void accept() throws IOException;
 }
--- src/com/vkopendoh/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/vkopendoh/webapp/storage/serializer/DataStreamSerializer.java
@@ -16,41 +16,50 @@ public class DataStreamSerializer implements SerializationStrategy {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
             Map<ContactType, String> contacts = resume.getContacts();
-            writeWithException(contacts.entrySet(), dos, entry -> {
+            Iterator<Map.Entry<ContactType, String>> it = contacts.entrySet().iterator();
+            writeWithException(contacts.entrySet(), dos, () -> {
+                Map.Entry<ContactType, String> entry = it.next();
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             });
             Map<SectionType, Section> sections = resume.getSections();
-            writeWithException(sections.entrySet(), dos, entry -> {
-                SectionType sectionType = entry.getKey();
-                dos.writeUTF(sectionType.name());
-                switch (sectionType) {
+            Iterator<Map.Entry<SectionType, Section>> sectionIterator = sections.entrySet().iterator();
+            writeWithException(sections.entrySet(), dos, () -> {
+                Map.Entry<SectionType, Section> sectionEntry = sectionIterator.next();
+                dos.writeUTF(sectionEntry.getKey().name());
+                switch (sectionEntry.getKey()) {
                     case PERSONAL:
                     case OBJECTIVE:
-                        TextSection textSection = (TextSection) entry.getValue();
+                        TextSection textSection = (TextSection) sectionEntry.getValue();
                         dos.writeUTF(textSection.getContent());
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        TextListSection section = (TextListSection) entry.getValue();
-                        List<String> content = section.getContent();
-                        writeWithException(content, dos, dos::writeUTF);
+                        TextListSection textListSection = (TextListSection) sectionEntry.getValue();
+                        Iterator<String> tlsIterator = textListSection.getContent().iterator();
+                        writeWithException(textListSection.getContent(), dos, () -> {
+                            String tlsEnry = tlsIterator.next();
+                            dos.writeUTF(tlsEnry);
+                        });
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
-                        OrganizationSection orgSection = (OrganizationSection) entry.getValue();
-                        List<Organization> contentOrg = orgSection.getContent();
-                        writeWithException(contentOrg, dos, org -> {
+                        OrganizationSection orgSection = (OrganizationSection) sectionEntry.getValue();
+                        Iterator<Organization> orgIterator = orgSection.getContent().iterator();
+                        writeWithException(orgSection.getContent(), dos, () -> {
+                            Organization org = orgIterator.next();
                             dos.writeUTF(org.getHomePage().getName());
                             dos.writeUTF(nullWriter(org.getHomePage().getUrl()));
-                            List<Organization.Experience> experiences = org.getExperiences();
-                            writeWithException(experiences, dos, exp -> {
+                            Iterator<Organization.Experience> expIterator = org.getExperiences().iterator();
+                            writeWithException(org.getExperiences(), dos, () -> {
+                                Organization.Experience exp = expIterator.next();
                                 dos.writeUTF(exp.getStartDate().format(dtf));
                                 dos.writeUTF(exp.getEndDate().format(dtf));
                                 dos.writeUTF(exp.getTitle());
                                 dos.writeUTF(nullWriter(exp.getDescription()));
                             });
                         });
+
                         break;
                 }
             });
@@ -61,8 +70,8 @@ public class DataStreamSerializer implements SerializationStrategy {
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
             Resume resume = new Resume(dis.readUTF(), dis.readUTF());
-            readWithException(dis, o -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
-            readWithException(dis, section -> {
+            readWithException(dis, () -> resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readWithException(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
                     case PERSONAL:
@@ -72,18 +81,18 @@ public class DataStreamSerializer implements SerializationStrategy {
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
                         List<String> content = new ArrayList<>();
-                        readWithException(dis, s -> content.add(dis.readUTF()));
+                        DataStreamSerializer.this.readWithException(dis, () -> content.add(dis.readUTF()));
                         resume.addSection(sectionType, new TextListSection(content));
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
                         List<Organization> organizations = new ArrayList<>();
-                        readWithException(dis, organization -> {
-                            Link homePage = new Link(dis.readUTF(), nullReader(dis.readUTF()));
+                        DataStreamSerializer.this.readWithException(dis, () -> {
+                            Link homePage = new Link(dis.readUTF(), DataStreamSerializer.this.nullReader(dis.readUTF()));
                             List<Organization.Experience> experiences = new ArrayList<>();
-                            readWithException(dis, o -> experiences.add(new Organization.Experience(YearMonth.parse(dis.readUTF(), dtf)
+                            DataStreamSerializer.this.readWithException(dis, () -> experiences.add(new Organization.Experience(YearMonth.parse(dis.readUTF(), dtf)
                                     , YearMonth.parse(dis.readUTF(), dtf)
-                                    , dis.readUTF(), nullReader(dis.readUTF()))));
+                                    , dis.readUTF(), DataStreamSerializer.this.nullReader(dis.readUTF()))));
                             organizations.add(new Organization(homePage, experiences));
                         });
                         resume.addSection(sectionType, new OrganizationSection(organizations));
@@ -102,11 +111,12 @@ public class DataStreamSerializer implements SerializationStrategy {
         return str.equals("") ? null : str;
     }
 
-    private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, ConsumerWithException<T> action) throws IOException {
+    private void writeWithException(Collection collection, DataOutputStream dos, ConsumerWithException
+            action) throws IOException {
         dos.writeInt(collection.size());
         Objects.requireNonNull(action);
-        for (T t : collection) {
-            action.accept(t);
+        for (Object o : collection) {
+            action.accept();
         }
     }
 
@@ -114,7 +124,7 @@ public class DataStreamSerializer implements SerializationStrategy {
         int size = dis.readInt();
         Objects.requireNonNull(action);
         for (int i = 0; i < size; i++) {
-            action.accept(null);
+            action.accept();
         }
     }
 }
