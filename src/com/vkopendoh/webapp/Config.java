0d0ec993e6997d49953e162854a24a2bfcb602c3
vkopendoh <vkopendoh@bk.ru>
2019-09-14 15:31:08 +0300

    Version for deployment on Heroku

--- src/com/vkopendoh/webapp/Config.java
+++ src/com/vkopendoh/webapp/Config.java
@@ -10,7 +10,8 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("config\\resumes.properties");
+    //private static final File PROPS = new File("/resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final String storageDir;
@@ -21,13 +22,13 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = props.getProperty("storage.dir");
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
